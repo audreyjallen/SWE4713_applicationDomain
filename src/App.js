3f64f20217ecf805318af4b8aa2016283ea34d96
Nate Hames <114621551+natehames@users.noreply.github.com>
2023-05-02 11:56:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,12 +11,21 @@ import {VerifyEmail} from './pages/VerifyEmail';
 import UserManagement from './app-pages/UserManagement';
 import DOB from './app-pages/calendar';
 import { Dashboard } from './app-pages/chartOfAccounts'
-import { AddAccount } from './app-pages/addAccount'
+import { AddAccount} from './app-pages/addAccount'
 import { EntryList } from './app-pages/journalEntryList'
 import { Ledger } from './app-pages/ledgerTemplate'
 import { AddJournal } from './app-pages/addJournalEntry'
-import { GeneralLedger } from './app-pages/generalLedger'
-import { ReviewJournal } from './app-pages/reviewJournals'
+import { ViewAccount } from './app-pages/ViewAccount';
+import { EditAccount } from './app-pages/EditAccount';
+import { Journal1 } from './app-pages/journal1';
+import { SelectUser } from './pages/selectUser';
+import { Create } from './pages/createManager';
+import { ManagerLog } from './pages/managerLog';
+import { AccountantLog } from './pages/accountantLog';
+import { CreateAccountant } from './pages/createAccountant';
+import { AccountantDashboard } from './app-pages/accountantchartOfAccounts';
+import { ManagerDashboard } from './app-pages/managerchartOfAccounts';
+
 
 
 function App() { 
@@ -24,27 +33,34 @@ function App() {
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
- 
   
   return (
     <div className="App">
        <UserAuthContext>
       <Router>
       <Routes>
-        <Route path="/" element={< Log />} />
+        <Route path="/" element={<SelectUser/>} />
+        <Route path="/log" element={< Log />} />
+        <Route path="/accountantLog" element={< AccountantLog />} />
+        <Route path="/managerLog" element={< ManagerLog />} />
         <Route path="/register" element={< Register />} />
+        <Route path="/createManager" element={< Create />}/>
+        <Route path="/createAccountant" element={< CreateAccountant />}/>
         <Route path="/reset" element={< Reset />} />
         <Route path="/verify" element={< Verify />} />
         <Route path="/verifyEmail" element={< VerifyEmail />} />
         <Route path="/Dashboard" element={< Dashboard />} />
+        <Route path="/ManagerDashboard" element={< ManagerDashboard />} />
+        <Route path="/AccountantDashboard" element={< AccountantDashboard />} />
         <Route path="/AddAccount" element={< AddAccount />} />
         <Route path="/UserManagement" element={< UserManagement />} />
         <Route path="/DOB" element={< DOB />} />
         <Route path="/Ledger" element={< Ledger />} />
         <Route path="/EntryList" element={< EntryList />} />
         <Route path="/AddJournal" element={< AddJournal />} />
-        <Route path="/GeneralLedger" element={< GeneralLedger />} />
-        <Route path="/ReviewJournal" element={< ReviewJournal />} />
+        <Route path="/ViewAccount" element={<ViewAccount />}/>
+        <Route path="/EditAccount" element={<EditAccount />}/>
+        <Route path="/Journal1" element={<Journal1 />}/>
     </Routes>
     </Router>
     </UserAuthContext>
