52495d17b3c36ca37f44f79ab9cb806a1290dcb7
Nate Hames <114621551+natehames@users.noreply.github.com>
2023-05-02 20:26:39 -0400

    Add files via upload

---
+++ src/app-pages/managerMenu.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { AppBar, Toolbar, Button } from '@mui/material'
+import { Tooltip } from '@mui/material'
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { HelpModal } from './helpModal';
+import  Calendar  from './calendar'
+import { useNavigate, Link } from 'react-router-dom'
+
+
+export const ManagerMenu = () => {
+
+    return (
+
+            <div className = "ContentNav">
+                <Tooltip title = "Calendar">
+                <div className = "CalendarButton">
+                <Calendar/>
+                </div>
+                </Tooltip>
+                <div className = "MidButton">
+                    <button>Home</button>
+                    <button>Email</button>
+                    <Link to ="/ChartofAccounts">
+                    <button>
+                        Charts of Accounts
+                    </button>
+                    </Link>
+                    <button>Journal</button>
+                    <button>Financial Reports</button>
+                </div>
+                <div className = "HelpButton">
+                      <HelpModal/>
+                </div>
+            </div>
+    )
+}
